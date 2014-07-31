81d958b9ec85db07ba443f2ae6f91d4f474394eb
EchPay <phuocnguyen286@gmail.com>
2014-07-31 15:00:08 +0700

    Remove old CI

--- Source/application/config/facebook.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-
-$config['appId']   = '695082060564419';
-$config['secret']  = '093b0b371673a8b831dcc87d62fee7b0';
-$config['facebook']['redirect_url'] = 'http://localhost/';
-
-?>
