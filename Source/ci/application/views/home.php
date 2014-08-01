0377c1d44f427efe2d338788c589c72dae5429e2
EchPay <nguyen.phuoc@mulodo.com>
2014-08-01 07:07:45 +0700

    Add new view

---
+++ Source/ci/application/views/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Tìm Chòi - Web application</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="<?php echo base_url(); ?>assets/css/sb-admin.css" rel="stylesheet">
+    
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">      
+    
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Hello, Tim Choi</h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
