1f7097551464bd0d5207082d8598814a2def3e17
Shin Nguyen <tatthien.hcmuc@gmail.com>
2014-08-05 14:59:01 +0700

    Some updates for map page

--- Source/ci/application/views/map.php
+++ Source/ci/application/views/map.php
@@ -22,46 +22,81 @@
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 
     <script>
-        function initialize() {
-            var locations = <?php echo $locations_json; ?>;
+        var locationAPI = "http://localhost/timchoi/index.php/api/location/loca";
 
-            var mapOptions = {
-                zoom: 16
-            };
+        $.getJSON( locationAPI, {
+            format: "json"
+        }).done(function(data) {
+            var locations = jQuery.parseJSON(data);
+        
+            function initialize(locations) {
+                var mapOptions = {
+                    zoom: 13
+                };
 
-            // show all location on map
-            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                // show all location on map
+                map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-            for(var i = 0; i < locations.length; i++) {
-                var marker = new google.maps.Marker ({
-                    position: new google.maps.LatLng(locations[i].lat, locations[i].long),
-                    map: map,
-                    title: locations[i].description
+                // infowindow
+                var infowindow = new google.maps.InfoWindow();
+
+                for(var i = 0; i < locations.length; i++) {
+                    // infowindow
+                    var contentString = '<div id="content">'+
+                      '<div id="siteNotice">'+
+                      '</div>'+
+                      '<h1 id="firstHeading" class="firstHeading">'+locations[i].name+'</h1>'+
+                      '<div id="bodyContent">'+
+                      '<p>'+locations[i].description+'</p>'+
+                      '</div>'+
+                      '</div>';
+
+                    var marker = new google.maps.Marker ({
+                        position: new google.maps.LatLng(locations[i].lat, locations[i].long),
+                        map: map,
+                        icon: "<?php echo base_url() . 'assets/images/dark-marker.png'; ?>",
+                        title: locations[i].name,
+                        info: contentString
+                    });
+
+                   
+
+                    // handle click event
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.setContent(this.info);
+                        infowindow.open(map, this);
+
+                        // zoom map
+                        //map.setZoom(15);
+                        //map.setCenter(this.getPosition());
+                    });
+                }
+
+                // get your current location using HTML5 Geolocation
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+                    var geolocate = new google.maps.LatLng(lat, long);
+
+                    //
+                    $('.geolocate').find('a').attr('data-lat', lat);
+                    $('.geolocate').find('a').attr('data-long', long);
+
+                    map.setCenter(geolocate);
+
+                    // marker 
+                    var market = new google.maps.Marker ({
+                        position: geolocate,
+                        map: map,
+                        icon: "<?php echo base_url() . 'assets/images/pink-marker.png'; ?>",
+                        title: "You are here"
+                    });
                 });
             }
 
-            // get your current location using HTML5 Geolocation
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var lat = position.coords.latitude;
-                var long = position.coords.longitude;
-                var geolocate = new google.maps.LatLng(lat, long);
-
-                //
-                $('.geolocate').find('a').attr('data-lat', lat);
-                $('.geolocate').find('a').attr('data-long', long);
-
-                map.setCenter(geolocate);
-
-                // marker 
-                var market = new google.maps.Marker ({
-                    position: geolocate,
-                    map: map,
-                    title: "You are here"
-                });
+            google.maps.event.addDomListener(window, 'load', initialize(locations));
+            
             });
-        }
-
-        google.maps.event.addDomListener(window, 'load', initialize);
     </script>
 
     <script>
